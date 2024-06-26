f22d7751bfaf33a7e3cc844e5aeb1a068fa2d654
Arthur Magomedov <devmagomedov@gmail.com>
2024-06-26 23:39:16 +0300

    minor changes

--- src/main/java/core/commands/CommandsList.java
+++ src/main/java/core/commands/CommandsList.java
@@ -15,6 +15,10 @@ public class CommandsList {
     public static final String LEZGI_ALPHABET = "\uD83D\uDD20Лезги гьарфарган";
 
     /* Old keypad commands */
+    /* These commands are needed because there are users who still have the old
+    version of Keypad (the front buttons for switching dictionaries). In order for
+    Keypad to update to the current version, it is necessary to read this old command,
+    then update the Keypad of the user. */
     public static final String LEZ_RUS_TAL = "\uD83D\uDCD9Лезгинско-русский словарь (Талибов Б., Гаджиев М.)";
     public static final String LEZ_RUS_BB = "\uD83D\uDCD7Лезгинско-русский словарь (Бабаханов М.Б.)";
     public static final String RUS_LEZ_GADZH = "\uD83D\uDCD5Русско-лезгинский словарь (Гаджиев М.М.)";
